6e83bfb3924b7ade2ed45d1aeb21ea053c7c1c29
Cainã Melo <caina.melo@fatec.sp.gov.br>
2024-10-14 20:46:53 -0300

    Feat: Insere tela do professor

--- tela_verificacao_aluno_professor/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module app.controllers {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens student to javafx.fxml;
-    opens app.controllers to javafx.fxml;
-    exports app.controllers;
-}
