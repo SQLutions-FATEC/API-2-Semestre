8c5eecadd085ee6925a0094c98037b5a86fbbae4
EnzoLFranco <fenzolemos@gmail.com>
2024-10-18 17:41:33 -0300

    fix: Muda URL na classe ConexaoBanco

--- src/main/java/app/controllers/ConexaoBanco.java
+++ src/main/java/app/controllers/ConexaoBanco.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoBanco {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/avaliador";
+    private static final String URL = "jdbc:mysql://localhost:3306/avaliador";
     private static final String USER = "root";
     private static final String PASSWORD = "admin";
 
