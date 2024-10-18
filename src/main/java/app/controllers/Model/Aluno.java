a194905d927bab9fbd6ff18eecd1c11ad4e54fe8
Enzo Lemos Franco <104728943+EnzoLFranco@users.noreply.github.com>
2024-10-17 21:47:37 -0300

    feat: Adiciona classe Aluno

---
+++ src/main/java/app/controllers/Model/Aluno.java
@@ -0,0 +1,62 @@
+package app.controllers.Model;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Aluno {
+
+    private SimpleIntegerProperty ra;
+    private SimpleStringProperty nome;
+    private SimpleStringProperty email;
+    private SimpleStringProperty senha;
+    private SimpleStringProperty id_equipe;
+
+    public Aluno(int ra, String nome, String email, String senha, String id_equipe) {
+        this.ra = new SimpleIntegerProperty(ra);
+        this.nome = new SimpleStringProperty(nome);
+        this.email = new SimpleStringProperty(email);
+        this.senha = new SimpleStringProperty(senha);
+        this.id_equipe = new SimpleStringProperty(id_equipe);
+    }
+
+    public Integer getRa() {
+        return ra.get();
+    }
+
+    public void setRa(Integer ra) {
+        this.ra = new SimpleIntegerProperty(ra);
+    }
+
+    public String getNome() {
+        return nome.get();
+    }
+
+    public void setNome(String nome) {
+        this.nome = new SimpleStringProperty(nome);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public void setEmail(String email) {
+        this.email = new SimpleStringProperty(email);
+    }
+
+    public String getSenha() {
+        return senha.get();
+    }
+
+    public void setSenha(String senha) {
+        this.senha = new SimpleStringProperty(senha);
+    }
+
+    public String getId_equipe() {
+        return id_equipe.get();
+    }
+
+    public void setId_equipe(String id_equipe) {
+        this.id_equipe = new SimpleStringProperty(id_equipe);
+    }
+
+}
