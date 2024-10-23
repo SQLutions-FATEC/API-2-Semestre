38cdad95aacc44574b47b832968067e110dc77c7
Tiago Torres <ttr100ttr100@gmail.com>
2024-10-23 19:55:10 -0300

    feat: adiciona tela pontuacao

--- src/main/java/app/controllers/ProfessorController.java
+++ src/main/java/app/controllers/ProfessorController.java
@@ -33,4 +33,13 @@ public class ProfessorController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void DefinirPontuacao(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/professor/SetScore.fxml"));
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
