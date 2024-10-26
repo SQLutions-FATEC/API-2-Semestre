0dceb546e20214a44d010fa3b035047ac3c01189
Glória Brito <gloriafbrito@gmail.com>
2024-10-26 17:23:53 -0300

    feat:adiciona model nota

--- src/main/java/app/models/NotaModel.java
+++ src/main/java/app/models/NotaModel.java
@@ -1,4 +1,29 @@
 package app.models;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class NotaModel {
+
+    private SimpleIntegerProperty idAluno;
+    private SimpleIntegerProperty idCriterio;
+    private SimpleDoubleProperty mediaNota;
+
+    public NotaModel(int idAluno, int idCriterio, double mediaNota) {
+        this.idAluno = new SimpleIntegerProperty(idAluno);
+        this.idCriterio = new SimpleIntegerProperty(idCriterio);
+        this.mediaNota = new SimpleDoubleProperty(mediaNota);
+    }
+
+    public int getIdAluno() {
+        return idAluno.get();
+    }
+
+    public int getIdCriterio() {
+        return idCriterio.get();
+    }
+
+    public double getMediaNota() {
+        return mediaNota.get();
+    }
 }
