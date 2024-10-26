5c42b4716027b7a2c36a1dd6c32fa37661e3e9ed
Glória Brito <gloriafbrito@gmail.com>
2024-10-26 17:22:15 -0300

    feat:acrescenta controler nota

--- src/main/java/app/controllers/NotaController.java
+++ src/main/java/app/controllers/NotaController.java
@@ -1,4 +1,36 @@
 package app.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class NotaController {
+
+    @FXML
+    private TableView<NotaModel> tableViewNotas;
+
+    @FXML
+    private TableColumn<NotaModel, Integer> colAluno;
+
+    @FXML
+    private TableColumn<NotaModel, Integer> colCriterio;
+
+    @FXML
+    private TableColumn<NotaModel, Double> colMediaNota;
+
+    public void initialize() {
+        colAluno.setCellValueFactory(new PropertyValueFactory<>("idAluno"));
+        colCriterio.setCellValueFactory(new PropertyValueFactory<>("idCriterio"));
+        colMediaNota.setCellValueFactory(new PropertyValueFactory<>("mediaNota"));
+    }
+
+    public void carregarDados(int idEquipe, int idSemestre, int idSprint, Integer idAluno) {
+        NotaDAO notaDAO = new NotaDAO();
+        List<NotaModel> notas = notaDAO.buscarNotas(idEquipe, idSemestre, idSprint, idAluno);
+        ObservableList<NotaModel> observableNotas = FXCollections.observableArrayList(notas);
+        tableViewNotas.setItems(observableNotas);
+    }
 }
