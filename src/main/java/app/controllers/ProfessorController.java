83725b4e7d3644cbae3341d9dffbd8459a5b5090
--global <--global>
2024-11-06 20:19:34 -0300

    fix: Resolve problemas com a chamada de tela

--- src/main/java/app/controllers/ProfessorController.java
+++ src/main/java/app/controllers/ProfessorController.java
@@ -9,10 +9,16 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
@@ -132,11 +138,10 @@ public class ProfessorController implements Initializable {
         Utils.setScreen(event, "setScore");
     }
     public void definirDataSprint(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/professor/setSprintData.fxml")));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/professor/setSprintData.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-        stage.setScene(scene);
+        stage.setScene(new Scene(root));
         stage.setTitle("Definir Data Sprint");
         stage.show();
     }
