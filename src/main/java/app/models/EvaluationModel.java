71f12b98ac32d6ea544cfa18d58116bf3c9c5bd2
Glória Brito <gloriafbrito@gmail.com>
2024-11-09 21:11:55 -0300

    feat: adiciona model historico de avaliação

--- src/main/java/app/models/EvaluationModel.java
+++ src/main/java/app/models/EvaluationModel.java
@@ -1,4 +1,49 @@
 package app.models;
+import app.models.EvaluationModel;
 
 public class EvaluationModel {
+    private String evaluatorName;       // Nome do avaliador
+    private int evaluatedStudentId;     // ID do avaliado
+    private String criteria;            // Critério de avaliação
+    private String sprintDescription;   // Descrição do sprint
+
+    public EvaluationModel(String evaluatorName, int evaluatedStudentId, String criteria, String sprintDescription) {
+        this.evaluatorName = evaluatorName;
+        this.evaluatedStudentId = evaluatedStudentId;
+        this.criteria = criteria;
+        this.sprintDescription = sprintDescription;
+    }
+
+    // Getters e setters para os campos
+    public String getEvaluatorName() {
+        return evaluatorName;
+    }
+
+    public void setEvaluatorName(String evaluatorName) {
+        this.evaluatorName = evaluatorName;
+    }
+
+    public int getEvaluatedStudentId() {
+        return evaluatedStudentId;
+    }
+
+    public void setEvaluatedStudentId(int evaluatedStudentId) {
+        this.evaluatedStudentId = evaluatedStudentId;
+    }
+
+    public String getCriteria() {
+        return criteria;
+    }
+
+    public void setCriteria(String criteria) {
+        this.criteria = criteria;
+    }
+
+    public String getSprintDescription() {
+        return sprintDescription;
+    }
+
+    public void setSprintDescription(String sprintDescription) {
+        this.sprintDescription = sprintDescription;
+    }
 }
