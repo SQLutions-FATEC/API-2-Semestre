5758689fc1667c07649ad2a7f81e72f3fe0c61ba
Glória Brito <gloriafbrito@gmail.com>
2024-11-09 21:10:44 -0300

    feat: adiciona controller do historico de visualização

--- src/main/java/app/controllers/StudentController.java
+++ src/main/java/app/controllers/StudentController.java
@@ -283,5 +283,18 @@ public class StudentController implements ScreenController {
     public void goToLoginScreen (ActionEvent event){
         Utils.setScreen(event, "loginScreen");
     }
+    @FXML
+    private void abrirHistoricoAvaliacoes(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/app/views/HistoricoAvaliacoes.fxml"));
+            Parent historicoRoot = loader.load();
+            Stage historicoStage = new Stage();
+            historicoStage.setScene(new Scene(historicoRoot));
+            historicoStage.setTitle("Histórico de Avaliações");
+            historicoStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
