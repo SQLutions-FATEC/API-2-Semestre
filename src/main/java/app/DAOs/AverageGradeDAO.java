af4e9c8039e6fc840d58085f144723d4f85f92d3
EnzoLFranco <fenzolemos@gmail.com>
2024-11-19 16:09:41 -0300

    fix: Modifica a query na String SQL no AverageGradeDAO

--- src/main/java/app/DAOs/AverageGradeDAO.java
+++ src/main/java/app/DAOs/AverageGradeDAO.java
@@ -12,22 +12,27 @@ public class AverageGradeDAO {
     private Map<String, AverageGradeModel> studentsMap = new HashMap<>();
 
     public Map<String, AverageGradeModel> fetchAverages(int teamId, int periodId, int sprintId) {
-        String sql = "SELECT u.nome AS usuario_nome, c.nome AS criterio, AVG(n.valor) AS media_nota FROM nota n " +
-                        "JOIN usuario u ON u.id = n.avaliado " +
-                        "JOIN periodo p ON p.id = n.periodo " +
-                        "JOIN sprint s ON s.id = n.sprint " +
-                        "JOIN criterio c ON n.criterio = c.id " +
-                        "WHERE p.id = ? AND s.id = ? AND u.equipe = ? " +
-                        "GROUP BY u.nome, c.nome";
 
-        try(ResultSet resultSet = DatabaseConnection.executeQuery(sql, periodId, sprintId, teamId)) {
+        String sql = "SELECT u.nome AS usuario_nome, c.nome AS criterio, " +
+                "COALESCE(SUM(n.valor), 0) AS media_nota, " +
+                "(SELECT COUNT(*) FROM usuario WHERE equipe = ?) AS total_users " +
+                "FROM usuario u " +
+                "LEFT JOIN nota n ON u.id = n.avaliado AND n.periodo = ? AND n.sprint = ? " +
+                "JOIN criterio c ON n.criterio = c.id " +
+                "WHERE u.equipe = ? " +
+                "GROUP BY u.nome, c.nome";
+
+
+        try (ResultSet resultSet = DatabaseConnection.executeQuery(sql, teamId, periodId, sprintId, teamId)) {
             while (resultSet.next()) {
                 String studentName = resultSet.getString("usuario_nome");
                 String criteria = resultSet.getString("criterio");
                 Double averageGrade = resultSet.getDouble("media_nota");
+                int totalUsers = resultSet.getInt("total_users");
 
+                double adjustedAverage = totalUsers > 0 ? averageGrade / totalUsers : 0;
                 AverageGradeModel student = studentsMap.getOrDefault(studentName, new AverageGradeModel(studentName));
-                student.setAverage(criteria, averageGrade);
+                student.setAverage(criteria, adjustedAverage);
                 studentsMap.put(studentName, student);
             }
         } catch (SQLException e) {
